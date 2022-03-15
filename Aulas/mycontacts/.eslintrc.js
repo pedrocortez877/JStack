2c8158f429fb2261876b6fc1c8f5122ece6d9aeb
Pedro Cortez <pedrocortez877@gmail.com>
2022-03-14 22:56:50 -0300

    Configurando eslint de forma correta

--- Aulas/mycontacts/.eslintrc.js
+++ Aulas/mycontacts/.eslintrc.js
@@ -1,15 +1,23 @@
 module.exports = {
   env: {
-    commonjs: true,
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier"],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 13,
+    sourceType: "module",
   },
   rules: {
+    "class-methods-use-this": "off", // desativa uma regra que diz que todo método de uma classe precisa usar a palavra this
+    "no-param-reassign": "off", // desativa uma regra que diz que não é permitido receber parâmetro e fazer alterações nele
+    camelcase: "off", // desativa uma regra que diz que toda variável precisa ser escrita no formato camelCase
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }], // não reclamar caso a variável utilizada seja next que é usada pelos middlewares do express
+    "prettier/prettier": "error",
+    "no-console": "off",
+    "consistent-return": "off",
+    "no-template-curly-in-string": "off",
+    "import/extensions": "off",
   },
 };
