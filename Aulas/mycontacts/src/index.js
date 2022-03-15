6c6b4f31f3fa2496b6dc8fa69f412d52b72258ff
Pedro Cortez <pedrocortez877@gmail.com>
2022-03-14 22:57:08 -0300

    Criando componente de rotas

--- Aulas/mycontacts/src/index.js
+++ Aulas/mycontacts/src/index.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
+const routes = require("./routes");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello word');
-});
+app.use(routes);
 
-app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
+app.listen(3000, () =>
+  console.log("🔥 Server started at http://localhost:3000")
+);
