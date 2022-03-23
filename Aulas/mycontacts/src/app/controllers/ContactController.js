028691637d55bbff357bf16d5f4a5c72d64e247d
Pedro Cortez <pedrocortez877@gmail.com>
2022-03-22 23:44:39 -0300

    feat: criando funcionalidades de show e delete

--- Aulas/mycontacts/src/app/controllers/ContactController.js
+++ Aulas/mycontacts/src/app/controllers/ContactController.js
@@ -6,13 +6,34 @@ class ContactController {
     response.json(contacts);
   }
 
-  show() {}
+  async show(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: "User not found" });
+    }
+
+    response.json(contact);
+  }
 
   store() {}
 
   update() {}
 
-  delete() {}
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: "User not found" });
+    }
+
+    await ContactsRepository.delete(id);
+    response.sendStatus(204);
+  }
 }
 
 module.exports = new ContactController();
--- Aulas/mycontacts/src/app/repositories/ContactsRepository.js
+++ Aulas/mycontacts/src/app/repositories/ContactsRepository.js
@@ -1,10 +1,17 @@
 const { v4 } = require("uuid");
 
-const contacts = [
+let contacts = [
   {
     id: v4(),
     name: "Pedro",
     email: "pedro@gmail.com",
+    phone: "982200861",
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: "José",
+    email: "José@gmail.com",
     phone: "982200861",
     category_id: v4(),
   },
@@ -13,6 +20,19 @@ const contacts = [
 class ContactsRepository {
   findAll() {
     return new Promise((resolve) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve) =>
+      resolve(contacts.find((contact) => contact.id === id))
+    );
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
   }
 }
 
