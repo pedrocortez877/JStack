838fc29b03cd850f71481fafceb509d57dce7d75
Pedro Cortez <pedrocortez877@gmail.com>
2022-03-22 23:20:42 -0300

    Alterando package do UUID

--- Aulas/mycontacts/src/app/repositories/ContactsRepository.js
+++ Aulas/mycontacts/src/app/repositories/ContactsRepository.js
@@ -1,12 +1,12 @@
-const { uuid } = require("uuidv4");
+const { v4 } = require("uuid");
 
 const contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: "Pedro",
     email: "pedro@gmail.com",
     phone: "982200861",
-    category_id: uuid(),
+    category_id: v4(),
   },
 ];
 
