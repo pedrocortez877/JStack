dd8a091f5e530923844e2d09e865c5be726c5458
Pedro Cortez <pedrocortez877@gmail.com>
2022-04-03 22:57:51 -0300

    feat: Refatorando metodo create

--- Aulas/mycontacts/src/app/repositories/ContactsRepository.js
+++ Aulas/mycontacts/src/app/repositories/ContactsRepository.js
@@ -1,4 +1,5 @@
 const { v4 } = require("uuid");
+const db = require("../../database");
 
 let contacts = [
   {
@@ -18,19 +19,17 @@ let contacts = [
 ];
 
 class ContactsRepository {
-  create({ name, email, phone, category_id }) {
-    return new Promise((resolve) => {
-      const newContact = {
-        id: v4(),
-        name,
-        email,
-        phone,
-        category_id,
-      };
+  async create({ name, email, phone, category_id }) {
+    const [row] = await db.query(
+      `
+        INSERT INTO contacts(name, email, phone, category_id)
+        VALUES($1, $2, $3, $4)
+        RETURNING *
+      `,
+      [name, email, phone, category_id]
+    );
 
-      contacts.push(newContact);
-      resolve(newContact);
-    });
+    return row;
   }
 
   update(id, { name, email, phone, category_id }) {
