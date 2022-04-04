9c22d2946630b07386e8d0ddeb8367fd849d9546
Pedro Cortez <pedrocortez877@gmail.com>
2022-04-03 23:05:59 -0300

    feat: Refatorando os metodos de find

--- Aulas/mycontacts/src/app/repositories/ContactsRepository.js
+++ Aulas/mycontacts/src/app/repositories/ContactsRepository.js
@@ -50,20 +50,21 @@ class ContactsRepository {
     });
   }
 
-  findAll() {
-    return new Promise((resolve) => resolve(contacts));
+  async findAll() {
+    const rows = await db.query("SELECT * FROM contacts");
+    return rows;
   }
 
-  findById(id) {
-    return new Promise((resolve) =>
-      resolve(contacts.find((contact) => contact.id === id))
-    );
+  async findById(id) {
+    const [row] = await db.query("SELECT * FROM contacts where id = $1", [id]);
+    return row;
   }
 
-  findByEmail(email) {
-    return new Promise((resolve) =>
-      resolve(contacts.find((contact) => contact.email === email))
-    );
+  async findByEmail(email) {
+    const [row] = await db.query("SELECT * FROM contacts where email = $1", [
+      email,
+    ]);
+    return row;
   }
 
   delete(id) {
