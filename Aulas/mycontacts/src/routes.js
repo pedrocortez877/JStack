92ae8ec73ab4136744f172e60468ae1602428571
Pedro Cortez <pedrocortez877@gmail.com>
2022-04-03 23:42:27 -0300

    feat: Criando o rotas de categorias

--- Aulas/mycontacts/src/routes.js
+++ Aulas/mycontacts/src/routes.js
@@ -1,12 +1,18 @@
 const { Router } = require("express");
 const ContactController = require("./app/controllers/ContactController");
+const CategoryController = require("./app/controllers/CategoryController");
 
 const router = Router();
 
+// ROTAS DE CONTATOS
 router.get("/contacts", ContactController.index);
 router.get("/contacts/:id", ContactController.show);
 router.delete("/contacts/:id", ContactController.delete);
 router.post("/contacts", ContactController.store);
 router.put("/contacts/:id", ContactController.update);
+
+//ROTAS DE CATEGORIAS
+router.post("/categories", CategoryController.store);
+router.get("/categories", CategoryController.index);
 
 module.exports = router;
