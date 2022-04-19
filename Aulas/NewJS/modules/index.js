0679e4cb6861732b168ebb601ca143572c4d8538
Pedro Cortez <pedrocortez877@gmail.com>
2022-04-18 23:00:33 -0300

    feat: esmodules

---
+++ Aulas/NewJS/modules/index.js
@@ -0,0 +1,10 @@
+function sayHello(name){
+  console.log(`Seja bem vindo ${name}`);
+}
+
+export default sayHello; //ESMODULE
+// MODULE.EXPORTS = sayHello //COMMONJS
+
+
+// export { sayHello } EXPORTANDO OBJETO SEPARADAMENTE, 
+// IRÁ OBRIGAR A IMPORTÁ-LO COM O MESMO NOME
