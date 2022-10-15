fe0f6b577d11c52a9de031c4c7589003c6bbd30b
Pedro Cortez <pedrocortez877@gmail.com>
2022-10-15 18:46:32 -0300

    Aulas 10-15

---
+++ Aulas/react-fundamentals/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1 id='title'>
+    <span>Olá mundo!</span>
+  </h1>,
+  document.getElementById('root'),
+)
---
+++ Aulas/react-fundamentals/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle[hash].js'
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      { 
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+     
+    ]
+  },
+  devServer: {
+    port: 3000,
+  }
+};
