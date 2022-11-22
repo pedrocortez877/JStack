7f06b4f7fd68e19262c279e9b12817ca8e6610af
Pedro Cortez <pedrocortez877@gmail.com>
2022-11-22 00:25:36 -0300

    feat: states in react

--- Aulas/react-fundamentals/src/App.js
+++ Aulas/react-fundamentals/src/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { Post } from './Post';
 import { Header } from './Header';
 
-const posts = [
-  {title: 'Title#01', subTitle: 'Sub#01', likes: 20},
-  {title: 'Title#02', subTitle: 'Sub#02', likes: 10},
-  {title: 'Title#03', subTitle: 'Sub#03', likes: 5},
-]
-
 export default function App(){
+  const [posts, setPosts] = useState([
+    {id: Math.random(), title: 'Title#01', subTitle: 'Sub#01', likes: 20},
+    {id: Math.random(), title: 'Title#02', subTitle: 'Sub#02', likes: 10},
+    {id: Math.random(), title: 'Title#03', subTitle: 'Sub#03', likes: 5},
+  ]);
+  
+  function handleRefresh(){
+    setPosts((prevState) => [
+        ...prevState,
+        {id: Math.random(), title: `Title#0${posts.length + 1}`, subTitle: `Sub#0${posts.length + 1}`, likes: 30}
+    ]);
+    console.log(posts);
+  }
+
   return (
     <>
       <Header title='Meu title do header'>
         <span>My Header</span>
+        <button onClick={handleRefresh}>Atualizar</button>
       </Header>
 
       {
@@ -24,7 +33,7 @@ export default function App(){
               title: post.title,
               subTitle: post.subTitle
             }}
-            key={index}
+            key={post.id}
           />
         ))
       }
--- Aulas/react-fundamentals/src/Post.js
+++ Aulas/react-fundamentals/src/Post.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-export function Post({post}){
+export function Post({post, likes}){
  return (
   <>
     <article>
       <strong>{post.title}</strong>
       <small>{post.subTitle}</small>
+      <small>Likes: {likes}</small>
     </article>
     <br />
   </>
